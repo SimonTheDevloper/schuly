1551a930247faf2088c1ace8dff51fc0a0baaaf1
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-12-01 20:51:48 +0100

    möglische datenstruchktur programmiert

---
+++ JS/daten.js
@@ -0,0 +1,29 @@
+const daten = {
+    fächer: [
+        {
+            fachname: "Mathe",
+            materialien: ["Mathebuch", "Theorieheft", "Übungsheft", "Taschenrechner", "Formelsammlung", "Zirkel",]
+        },
+        {
+            fachname: "Englisch",
+            materialien: ["Englischbuch", "Übungsheft"]
+        },
+        {
+            fachname: "IT",
+            materialien: ["IT-Buch", "USB-Stick"]
+        }
+    ],
+    stundenplan: [
+        {
+            WochenTag: "Montag",
+            fächer: ["Mathe", "Englisch"]
+        },
+        {
+            WochenTag: "Dienstag",
+            fächer: ["IT", "Englisch"]
+        }
+
+    ]
+
+
+}
