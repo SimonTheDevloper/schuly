137024563570594c00a89869624a628563a5fc55
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-12-02 18:13:25 +0100

    bei dem script.js beim Testen gecodet das das Datum herausgefunden wird und daraus werden die benötigten materialien aufgelistet die man braucht

--- Testen/script.js
+++ Testen/script.js
@@ -21,10 +21,33 @@ const daten = {
         {
             WochenTag: "Dienstag",
             fächer: ["IT", "Englisch"]
-        }
-
+        },
     ]
 }
 
-const heute = new Date();
-console.log(heute); 
+const heuteDatum = new Date();
+const heuteWochentag = heuteDatum.toLocaleDateString("de-DE", { weekday: "long" });
+//console.log(heuteWochentag);
+
+
+let alleWochentage = daten.stundenplan
+
+let einWochentag = alleWochentage.filter(objekt => objekt.WochenTag === heuteWochentag);
+let fächerFürEinenTag = einWochentag.map(tag => tag.fächer).flat()
+
+console.log(fächerFürEinenTag)
+
+//console.log(alleWochentage)
+
+
+const alleFächer = daten.fächer; console.log()
+
+// Filtert alle Fächer, deren Name in der Liste der gewünschten Fächer ('fächerFürEinenTag') enthalten ist.
+const fachObjektZumFach = alleFächer.filter(fach =>
+    fächerFürEinenTag.includes(fach.fachname)
+)
+const materialienZumFach = fachObjektZumFach.map(t => t.materialien).flat()
+console.log(materialienZumFach)
+
+
+
